68d33ecf00c96c7fa7f8c95e33b16807157a21f3
PoffM <matp@live.ca>
2018-04-24 16:17:20 -0400

    Feedback Support #11911 Create SpringBoot project (#1)
    Feedback Support #11911 Create SpringBoot project

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Sequence management services managing laboratory workflows leading to DNA sequences.
 
 seqdb-api is an implementation of the Sequence Module for the [DINA project](https://www.dina-project.net/).
+
+
+## Required:
+
+Java 1.8+
+
+Maven 3.2+
+
+## To launch:
+
+mvn spring-boot:run
---
+++ src/main/java/ca/gc/aafc/seqdb/api/SeqdbApiLauncher.java
@@ -0,0 +1,16 @@
+package ca.gc.aafc.seqdb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Launches the application.
+ */
+@SpringBootApplication
+public class SeqdbApiLauncher {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeqdbApiLauncher.class, args);
+  }
+
+}

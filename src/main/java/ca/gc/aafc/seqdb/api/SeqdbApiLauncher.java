bb7d64ae0ff10c95b3c47df123bc160913f48961
poffm <poffm@ONOTTRA670225P.AGR.GC.CA>
2018-04-24 14:42:42 -0400

    Feedback Support #11912 Add Hibernate and SeqDB dbi module
    https://redmine.biodiversity.agr.gc.ca/issues/11912
    
    -Added Hibernate, H2, Mysql dependencies in the pom.
    -Added the dbi jar in an included lib folder because dbi is not on any
    public maven repo.
    -Added application.yml with hibernate config to get this working with
    the dbi models.

--- src/main/java/ca/gc/aafc/seqdb/api/SeqdbApiLauncher.java
+++ src/main/java/ca/gc/aafc/seqdb/api/SeqdbApiLauncher.java
@@ -2,12 +2,14 @@ package ca.gc.aafc.seqdb.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * Launches the application.
  * @author poffm
  */
 @SpringBootApplication
+@EntityScan("ca.gc.aafc.seqdb.entities")
 public class SeqdbApiLauncher {
 
   public static void main(String[] args) {

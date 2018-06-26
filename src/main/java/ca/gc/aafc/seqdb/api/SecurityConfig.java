7c858d448578c798e6e84befc0d36e677960d332
poffm <poffm@ONOTTRA670225P.AGR.GC.CA>
2018-06-26 17:04:55 -0400

    In progress Feature #11916 Design authentication support
    https://redmine.biodiversity.agr.gc.ca/issues/11916
    
    -Added spring-security-ldap dependency to the pom.
    -Added Auth using ldap: Trying to access the API while unauthenticated
    gives you the default Spring login page. Logging in gives you full
    access to the data.

--- src/main/java/ca/gc/aafc/seqdb/api/SecurityConfig.java
+++ src/main/java/ca/gc/aafc/seqdb/api/SecurityConfig.java
@@ -1,16 +1,45 @@
 package ca.gc.aafc.seqdb.api;
 
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ldap.LdapProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.AbstractContextSource;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Inject
+  private ContextSource contextSource;
+  
+  @Inject
+  private LdapProperties ldapProperties;
+  
+  @Value("${spring.ldap.searchBase:}")
+  private String ldapSearchBase;
+  
+  @Value("${spring.ldap.searchFilter:}")
+  private String ldapSearchFilter;
+  
   @Override
-  public void configure(WebSecurity web) throws Exception {
-    // Disables authentication
-    web.ignoring().antMatchers("/**");
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // If an LDAP URL is specified then enable LDAP auth.
+    if (ldapProperties.getUrls() != null) {
+      // Set "referral" to "follow" to avoid PartialResultException.
+      ((AbstractContextSource) contextSource).setReferral("follow");
+      ((AbstractContextSource) contextSource).afterPropertiesSet();
+      
+      auth.
+          ldapAuthentication()
+              .userSearchBase(ldapSearchBase)
+              .userSearchFilter(ldapSearchFilter)
+              .contextSource((BaseLdapPathContextSource) contextSource);
+    }
   }
-
+  
 }

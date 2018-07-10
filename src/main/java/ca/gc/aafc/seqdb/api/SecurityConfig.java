0dbc09642ed0d45441a1d8ae2834310c044671f6
poffm <poffm@ONOTTRA670225P.AGR.GC.CA>
2018-07-10 16:16:56 -0400

    In progress Feature #12840 Authentication with Keycloak
    https://redmine.biodiversity.agr.gc.ca/issues/12840
    
    Added basic authentication with Keycloak:
    -Added "keycloak-spring-boot-starter" dependency.
    -Added a separate WebSecurityConfigurerAdapter for Keycloak that is used
    instead when the property "keycloak.enabled" is true.
    -Added keycloak.example.yml as an example of a keycloak config.
    -Changed a line in ReadableGroupFilterHandler to get the current
    account's name that now works with local, ldap and keycloak accounts.
    SecurityContextHolder.getContext().getAuthentication().getName() for a
    Keycloak accounts returns Keycloak's ID for that account (e.g.
    87959e4d-a07a-4083-a383-79149ad9a138).
    
    Visiting a URL for this application redirects you to a Keycloak login
    page. Logging in redirects you back to the URL you tried to access.
    
    The application currently does not create a new DB Account record when a
    Keycloak user logs in.

--- src/main/java/ca/gc/aafc/seqdb/api/SecurityConfig.java
+++ src/main/java/ca/gc/aafc/seqdb/api/SecurityConfig.java
@@ -2,7 +2,12 @@ package ca.gc.aafc.seqdb.api;
 
 import javax.inject.Inject;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.ldap.LdapProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +16,14 @@ import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.support.AbstractContextSource;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 import ca.gc.aafc.seqdb.api.security.SeqdbDaoAuthenticationProvider;
 import ca.gc.aafc.seqdb.api.security.SeqdbLdapUserDetailsMapper;
@@ -26,39 +36,40 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-  
+
   @Configuration
+  @ConditionalOnProperty(value = "keycloak.enabled", havingValue = "false")
   public static class WebSecurityConfigurerAdapterImpl extends WebSecurityConfigurerAdapter {
-    
+
     @Inject
     private ContextSource contextSource;
-    
+
     @Inject
     private LdapProperties ldapProperties;
-    
+
     @Value("${spring.ldap.searchBase:}")
     private String ldapSearchBase;
-    
+
     @Value("${spring.ldap.searchFilter:}")
     private String ldapSearchFilter;
-    
+
     @Inject
     private SeqdbDaoAuthenticationProvider seqdbAuthenticationProvider;
-    
+
     @Inject
     private SeqdbLdapUserDetailsMapper seqdbLdapUserDetailsMapper;
-    
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
       auth
           .authenticationProvider(seqdbAuthenticationProvider);
-      
+
       // If an LDAP URL is specified then enable LDAP auth.
       if (ldapProperties.getUrls() != null) {
         // Set "referral" to "follow" to avoid PartialResultException.
         ((AbstractContextSource) contextSource).setReferral("follow");
         ((AbstractContextSource) contextSource).afterPropertiesSet();
-        
+
         auth
             .ldapAuthentication()
             .userSearchBase(ldapSearchBase)
@@ -67,7 +78,41 @@ public class SecurityConfig {
             .contextSource((BaseLdapPathContextSource) contextSource);
       }
     }
-    
+
   }
-  
+
+  @Configuration
+  @ConditionalOnProperty(value = "keycloak.enabled", matchIfMissing = true)
+  public static class KeycloakWebSecurityConfigurerAdapterImpl
+      extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Inject
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      KeycloakAuthenticationProvider keycloakAuthProvider = keycloakAuthenticationProvider();
+      keycloakAuthProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+      auth.authenticationProvider(keycloakAuthProvider);
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+      return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+      return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+      super.configure(http);
+      http
+          .authorizeRequests()
+          .anyRequest().authenticated();
+    }
+
+  }
+
 }

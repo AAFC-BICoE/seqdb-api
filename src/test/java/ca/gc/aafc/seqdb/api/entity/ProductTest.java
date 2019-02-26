52a6fca4a1dc830bcb3a135030ec739704d60993
xuemeigan <xuemei.gan@canada.ca>
2019-02-26 13:37:35 -0500

    Feature 15088 Expose Product - Facilitate integration test with rest-assured and preemptive auth.

--- src/test/java/ca/gc/aafc/seqdb/api/entity/ProductTest.java
+++ src/test/java/ca/gc/aafc/seqdb/api/entity/ProductTest.java
@@ -7,18 +7,14 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,11 +24,10 @@ import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
 
-import ca.gc.aafc.seqdb.api.SeqdbApiLauncher;
 import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SeqdbApiLauncher.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration
 @Transactional
 public class ProductTest extends TestCase {
@@ -42,17 +37,18 @@ public class ProductTest extends TestCase {
   @PersistenceContext
   protected EntityManager entityManager;  
   
-  
   private RequestSpecification givenAuth() {
-    return RestAssured.given()
+    return RestAssured.given().port(port)
         .auth().preemptive()
-        .basic("reader", "reader");
+        .basic("Admin", "Admin");
  }
+  
+  @LocalServerPort
+  private int port;
   
   @Before
   public void setup() {
     endPoint = "/api";
- 
   }
   
   @Test
@@ -107,4 +103,5 @@ public class ProductTest extends TestCase {
     givenAuth().contentType("application/vnd.api+json").body(dataMap).when().patch(endPoint + "/product/1").then()
               .statusCode(HttpStatus.SC_OK);
   }  
+ 
 }

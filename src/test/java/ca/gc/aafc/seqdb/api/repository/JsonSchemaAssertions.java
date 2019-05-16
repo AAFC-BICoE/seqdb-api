a322b8ab67147448b9e2382d8cd98246efc3b2d3
Brandon Andre <andr0284@algonquinlive.com>
2019-05-16 11:25:45 -0400

    Support #16133 - JSON Schema Redundancy Cleanup
    https://redmine.biodiversity.agr.gc.ca/issues/16133
    
    - Created region and thermocycler attribute JSON files.
    - Edited all of the $ids of the current schemas to point in the right
    spot.
    - Edited the JsonSchemaAssertions with code to resolve references in the
    schema. Now points to the files located in the /json-schema folder.

--- src/test/java/ca/gc/aafc/seqdb/api/repository/JsonSchemaAssertions.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/JsonSchemaAssertions.java
@@ -1,14 +1,23 @@
 package ca.gc.aafc.seqdb.api.repository;
 
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.function.Consumer;
 
 import javax.json.stream.JsonParser;
 
 import org.leadpony.justify.api.JsonSchema;
+import org.leadpony.justify.api.JsonSchemaReader;
+import org.leadpony.justify.api.JsonSchemaReaderFactory;
+import org.leadpony.justify.api.JsonSchemaResolver;
 import org.leadpony.justify.api.JsonValidationService;
 import org.leadpony.justify.api.ProblemHandler;
 
@@ -34,10 +43,20 @@ public class JsonSchemaAssertions {
   public static void assertJsonSchema(Reader jsonSchema, Reader apiResponse) throws IOException {
 
     JsonValidationService service = JsonValidationService.newInstance();
+ 
+    // Create a custom JSON reader
+    JsonSchemaReaderFactory factory = service.createSchemaReaderFactoryBuilder()
+        .withSchemaResolver(new LocalJsonSchemaResolver(service)).build();
+    
     // Reads the JSON schema
-    JsonSchema schema = service.readSchema(jsonSchema);
+    JsonSchemaReader reader = factory.createSchemaReader(jsonSchema);
+    
+    // Generate the schema object from the reader.
+    JsonSchema schema = reader.read();
+
     // Problem handler which will print problems found.
     ProblemHandler handler = service.createProblemPrinter(ASSERTION_PRINTER);
+    
     // Parses the JSON instance by javax.json.stream.JsonParser
     try (JsonParser parser = service.createParser(apiResponse, schema, handler)) {
       while (parser.hasNext()) {
@@ -46,4 +65,44 @@ public class JsonSchemaAssertions {
     }
 
   }
+  
+  /**
+   * Schema resolver which looks for $ref locations in our resources where all of
+   * schemas are located.
+   */
+  private static class LocalJsonSchemaResolver implements JsonSchemaResolver {
+
+    JsonValidationService service;
+    
+    public LocalJsonSchemaResolver(JsonValidationService service) {
+      this.service = service;
+    }
+    
+    @Override
+    public JsonSchema resolveSchema(URI id) {
+      Path path = null;
+      
+      // Try to find the path based on where all of the schemas are located.
+      try {
+        
+        // Grab the resource the schema is trying to reference.
+        URL resource = ClassLoader.getSystemResource(id.getPath());
+        
+        // Check if it exists. If no resource is found, return no schema back.
+        if (resource == null) {
+          return null;
+        }
+
+        // Get the schemas path based on the resource given.
+        path = Paths.get(resource.toURI());
+        
+      } catch (URISyntaxException e) {
+        // URI had a syntax issue and cannot be resolved.
+        return null;
+      }
+      
+      // Read the schema located at this location as the reference.
+      return service.readSchema(path);
+    }
+  };
 }

5609cad3e06255f124bc69b1f38c1e740fcf5f91
jun <jun@jun-VirtualBox>
2019-05-18 00:48:17 -0400

    Add test to verify authentication using sample account "Admin/Admin". Enable testing account by adding Environment Variable "import-sample-account"

--- src/test/java/ca/gc/aafc/seqdb/api/TomcatWebServerCustomizerIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/TomcatWebServerCustomizerIT.java
@@ -1,7 +1,10 @@
 package ca.gc.aafc.seqdb.api;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -23,6 +26,22 @@ public class TomcatWebServerCustomizerIT extends BaseHttpIntegrationTest {
     HttpResponse response = client.execute(request);
     // Expect status code 401 unauthorized, instead of 400 for illegal square brackets.
     assertEquals(401, response.getStatusLine().getStatusCode());
+  }
+  
+  @Test
+  public void sendRequestToRegionEndpoint_withAuthentication_statusCode200() throws ClientProtocolException, IOException {
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpGet request = new HttpGet("http://localhost:8080/api/region?page[limit]=10");
+    
+    String auth = "Admin:Admin";
+    
+    byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
+    String authHeader = "Basic " + new String(encodedAuth);
+    request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+    
+    HttpResponse response = client.execute(request);
+    // Expect status code 200 unauthorized.
+    assertEquals(200, response.getStatusLine().getStatusCode());
   }
   
 }

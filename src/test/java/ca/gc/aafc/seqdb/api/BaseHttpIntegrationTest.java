c19be4f4419a389226ce99cd05196f30b63087a3
jun <jun@jun-VirtualBox>
2019-05-30 22:46:55 -0400

    add @TestPropertySource(properties="import-sample-accounts=true") at class BaseHttpIntegrationTest

--- src/test/java/ca/gc/aafc/seqdb/api/BaseHttpIntegrationTest.java
+++ src/test/java/ca/gc/aafc/seqdb/api/BaseHttpIntegrationTest.java
@@ -1,10 +1,12 @@
 package ca.gc.aafc.seqdb.api;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(
     classes = SeqdbApiLauncher.class,
     webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
     properties = "server.port=8080"
 )
+@TestPropertySource(properties="import-sample-accounts=true")
 public abstract class BaseHttpIntegrationTest extends BaseIntegrationTest { }

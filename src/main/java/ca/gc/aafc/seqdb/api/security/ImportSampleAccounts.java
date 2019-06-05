4250fd5da7c7668417b7dc23fe7e4d0d83f44039
KarimTahan <taha0025@algonquinlive.com>
2019-06-05 15:19:12 -0400

    Support #16310 Upgrade seqdb-api dbi version
    https://redmine.biodiversity.agr.gc.ca/issues/16310
    
    * Changed ImportSampleAccounts to use Sessions

--- src/main/java/ca/gc/aafc/seqdb/api/security/ImportSampleAccounts.java
+++ src/main/java/ca/gc/aafc/seqdb/api/security/ImportSampleAccounts.java
@@ -2,9 +2,12 @@ package ca.gc.aafc.seqdb.api.security;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -13,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import ca.gc.aafc.seqdb.entities.Account;
 import ca.gc.aafc.seqdb.entities.AccountsGroup;
 import ca.gc.aafc.seqdb.entities.Group;
+import ca.gc.aafc.seqdb.service.GroupManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,50 +33,62 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class ImportSampleAccounts implements ApplicationListener<ContextRefreshedEvent> {
 
-  private final EntityManager entityManager;
+  private final EntityManagerFactory emf;
   
   private final PasswordEncoder passwordEncoder;
+  
+  private Session session;
+  
+  private GroupManager groupManager;
   
   @Transactional
   @Override
   public void onApplicationEvent(ContextRefreshedEvent arg0) {
     log.info("Importing sample accounts...");
     
+    session = emf.unwrap(SessionFactory.class).openSession();
+    groupManager = new GroupManager();
+    groupManager.setSessionFactory(session.getSessionFactory());
+    Transaction transaction = session.beginTransaction();
+    
     Account adminAccount = new Account();
     adminAccount.setAccountName("Admin");
     adminAccount.setAccountPw(passwordEncoder.encode("Admin"));
     adminAccount.setAccountType("Admin");
     adminAccount.setAccountStatus("Active");
-    entityManager.persist(adminAccount);
+    session.persist(adminAccount);
     
     Group adminGroup = new Group();
     adminGroup.setGroupName("Admin");
-    entityManager.persist(adminGroup);
+    session.persist(adminGroup);
     
     AccountsGroup adminPermissions = new AccountsGroup();
     adminPermissions.setAccount(adminAccount);
     adminPermissions.setGroup(adminGroup);
     adminPermissions.setRights("1111");
     adminPermissions.setAdmin(true);
-    entityManager.persist(adminPermissions);
+    session.persist(adminPermissions);
     
     Account userAccount = new Account();
     userAccount.setAccountName("User");
     userAccount.setAccountPw(passwordEncoder.encode("User"));
     userAccount.setAccountType("User");
     userAccount.setAccountStatus("Active");
-    entityManager.persist(userAccount);
+    session.persist(userAccount);
     
     Group userGroup = new Group();
     userGroup.setGroupName("User");
-    entityManager.persist(userGroup);
+    session.persist(userGroup);
     
     AccountsGroup userPermissions = new AccountsGroup();
     userPermissions.setAccount(userAccount);
     userPermissions.setGroup(userGroup);
     userPermissions.setRights("1111");
     userPermissions.setAdmin(true);
-    entityManager.persist(userPermissions);
+    session.persist(userPermissions);
+    
+    transaction.commit();
+    session.close();
     
     log.info("Imported sample accounts.");
   }

8d174d53fe845acf1b4032cd7b5c701019dd3746
PoffM <matp@live.ca>
2019-06-17 15:59:11 -0400

    Feedback Support #16358 Make JSON Schemas resolvable (#62)
    -Moved the json schema files to a static directory.
    -Added section to docs about static access of the schema files.
    -Changed the paths to the schema files to fix the failing tests.

--- src/test/java/ca/gc/aafc/seqdb/api/repository/PCRBatchResourceRepositoryIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/PCRBatchResourceRepositoryIT.java
@@ -9,14 +9,14 @@ public class PCRBatchResourceRepositoryIT {
   @Test
   public void listRegion_APIResponse_schemaValidates() throws IOException {
     JsonSchemaAssertions.assertJsonSchema(
-        BaseRepositoryTest.newClasspathResourceReader("json-schema/GETpcrbatchJSONSchema.json"),
+        BaseRepositoryTest.newClasspathResourceReader("static/json-schema/GETpcrbatchJSONSchema.json"),
         BaseRepositoryTest.newClasspathResourceReader("realPcrbatchResponse-all.json"));
   }
 
   @Test
   public void getRegion_APIResponse_schemaValidates() throws IOException {
     JsonSchemaAssertions.assertJsonSchema(
-        BaseRepositoryTest.newClasspathResourceReader("json-schema/pcrbatchJSONSchema.json"),
+        BaseRepositoryTest.newClasspathResourceReader("static/json-schema/pcrbatchJSONSchema.json"),
         BaseRepositoryTest.newClasspathResourceReader("realPcrbatchResponse.json"));
   }
 }

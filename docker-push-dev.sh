f93b815f869dd3299d82bb159e0bfac9f66df630
poffm <matp@live.ca>
2019-06-28 13:55:06 -0400

    In progress Support #16529 Containerize seqdb-api and deploy on dev server
    https://redmine.biodiversity.agr.gc.ca/issues/16529
    
    Added travis config and script to push docker images for this branch.

---
+++ docker-push-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the app version from pom.xml
+MVN_VERSION=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+
+# The name and tag of the docker image. "dev-" is prepended to the tag.
+DOCKER_IMAGE_FULLNAME="$DOCKER_IMAGE":dev-"$MVN_VERSION"
+
+docker build -t "$DOCKER_IMAGE_FULLNAME"
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Push the image to Docker Hub
+docker push "$DOCKER_IMAGE_FULLNAME"

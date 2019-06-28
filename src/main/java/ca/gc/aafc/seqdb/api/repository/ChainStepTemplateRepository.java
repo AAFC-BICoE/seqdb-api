71a755773a7eacc6b46dfb7a13c496fe9f38c873
Brandon Andre <andr0284@algonquinlive.com>
2019-06-28 13:01:14 -0400

    Feature #14656 Add workflow entity support
    https://redmine.biodiversity.agr.gc.ca/issues/14656
    
    - Added the JPA Repositories for each of the new entities.

---
+++ src/main/java/ca/gc/aafc/seqdb/api/repository/ChainStepTemplateRepository.java
@@ -0,0 +1,28 @@
+package ca.gc.aafc.seqdb.api.repository;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Component;
+
+import ca.gc.aafc.seqdb.api.dto.ChainStepTemplateDto;
+import ca.gc.aafc.seqdb.api.repository.filter.RsqlFilterHandler;
+import ca.gc.aafc.seqdb.api.repository.filter.SimpleFilterHandler;
+import ca.gc.aafc.seqdb.api.repository.jpa.JpaDtoRepository;
+import ca.gc.aafc.seqdb.api.repository.jpa.JpaResourceRepository;
+import ca.gc.aafc.seqdb.api.repository.meta.JpaMetaInformationProvider;
+import ca.gc.aafc.seqdb.api.security.authorization.ReadableGroupFilterHandlerFactory;
+
+import lombok.NonNull;
+
+@Component
+public class ChainStepTemplateRepository extends JpaResourceRepository<ChainStepTemplateDto> {
+
+  public ChainStepTemplateRepository(@NonNull JpaDtoRepository dtoRepository, SimpleFilterHandler simpleFilterHandler,
+      RsqlFilterHandler rsqlFilterHandler, ReadableGroupFilterHandlerFactory groupFilterFactory,
+      JpaMetaInformationProvider metaInformationProvider) {
+    
+    super(ChainStepTemplateDto.class, dtoRepository, Arrays.asList(simpleFilterHandler), metaInformationProvider);
+    
+  }
+  
+}

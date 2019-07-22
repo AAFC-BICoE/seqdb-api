1f8ea5f296109a6c4c9d624d75ac4286001dccba
Christian Gendreau <christiangendreau@gmail.com>
2019-07-22 10:47:06 -0400

    Support #16027 Create JSONSchema for Protocol and ReactionComponent Catch JSONException to try to get more information about test failures.

--- src/test/java/ca/gc/aafc/seqdb/api/repository/JsonSchemaAssertions.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/JsonSchemaAssertions.java
@@ -14,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
 
+import javax.json.JsonException;
 import javax.json.stream.JsonParser;
 
 import org.apache.http.client.utils.URIBuilder;
@@ -93,7 +94,10 @@ public class JsonSchemaAssertions {
       while (parser.hasNext()) {
         parser.next();
       }
+    } catch (JsonException jsonEx) {
+      fail("Trying to assert schema located at " + uri.toString() + ": " + jsonEx.getMessage());
     }
+    
   }
 
   /**

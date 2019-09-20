4d237a4ab6c460fd124d8d19d3ffd8de93cf44eb
poffm <matp@live.ca>
2019-09-20 11:29:42 -0400

    In progress Support #17364 Expose Container, ContainerType and Location
    https://redmine.biodiversity.agr.gc.ca/issues/17364
    
    -Added Row/Column validation to LocationRepository.
    -Added location validation tests.

--- src/main/java/ca/gc/aafc/seqdb/api/repository/LocationRepository.java
+++ src/main/java/ca/gc/aafc/seqdb/api/repository/LocationRepository.java
@@ -1,9 +1,14 @@
 package ca.gc.aafc.seqdb.api.repository;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import javax.persistence.EntityManager;
+import javax.validation.ValidationException;
 
 import org.springframework.stereotype.Component;
 
+import ca.gc.aafc.seqdb.NumberLetterMappingUtils;
 import ca.gc.aafc.seqdb.api.dto.LocationDto;
 import ca.gc.aafc.seqdb.api.repository.filter.RsqlFilterHandler;
 import ca.gc.aafc.seqdb.api.repository.filter.SimpleFilterHandler;
@@ -11,6 +16,8 @@ import ca.gc.aafc.seqdb.api.repository.jpa.JpaDtoRepository;
 import ca.gc.aafc.seqdb.api.repository.jpa.JpaResourceRepository;
 import ca.gc.aafc.seqdb.api.repository.meta.JpaMetaInformationProvider;
 import ca.gc.aafc.seqdb.api.security.authorization.ReadableGroupFilterHandlerFactory;
+import ca.gc.aafc.seqdb.entities.Container;
+import ca.gc.aafc.seqdb.entities.ContainerType;
 
 @Component
 public class LocationRepository extends JpaResourceRepository<LocationDto> {
@@ -24,4 +31,55 @@ public class LocationRepository extends JpaResourceRepository<LocationDto> {
         metaInformationProvider);
   }
 
+  @Override
+  public <S extends LocationDto> S save(S resource) {
+    this.validateCoordinates(resource);
+    return super.save(resource);
+  }
+
+  @Override
+  public <S extends LocationDto> S create(S resource) {
+    this.validateCoordinates(resource);
+    return super.create(resource);
+  }
+  
+  private void validateCoordinates(LocationDto location) {
+    EntityManager em = this.dtoRepository.getEntityManager();
+    // is container id always available?
+    Container container = em.find(
+        Container.class,
+        location.getContainer().getContainerId()
+    );
+    
+    ContainerType cType = container.getContainerType();
+
+    Integer rows = cType.getNumberOfRows();
+    Integer cols = cType.getNumberOfColumns();
+
+    String row = location.getWellRow();
+    Integer col = location.getWellColumn();
+    
+    // Checks that the coordinates match the integer followed by letter pattern
+    if (!Pattern.matches("[a-zA-Z]*", row)) {
+      throw new ValidationException("Well row must be in a letter format. (e.g: D)");
+    }
+
+    // Checks that the well column number is not 0
+    if (col <= 0) {
+      throw new ValidationException(String.format("Well column %s is less than 1.", col));
+    }
+    
+    if (col > cols) {
+      throw new ValidationException(
+          String.format("Well column %s exceeds container's number of columns.", col)
+      );
+    }
+    
+    if (NumberLetterMappingUtils.getNumber(row) > rows) {
+      throw new ValidationException(
+          String.format("Row letter %s exceeds container's number of rows.", row)
+      );
+    }
+  }
+  
 }

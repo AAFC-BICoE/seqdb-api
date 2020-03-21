4354f5df9098907190870035b6c817f35df7e71a
Chalker, Tom <tom.chalker@canada.ca>
2020-03-21 11:16:31 -0400

    Replaced spring.datasource.url with constructed value

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -11,8 +11,10 @@ if [ 0 != $? ]; then
    echo "creating sample accounts"
    export ARGS="-Dimport-sample-accounts=true"
 fi
-
-echo "url=$(echo '$spring.datasource.url' | awk  -f envSubstitution.awk)"
+# Need to build the spring.datasource.url as database and schema name may be set differently in the Env Var settings.
+SCHEMA=$(echo '$spring.liquibase.defaultSchema' | awk -f envSubstitution.awk)
+env "spring.datasource.url=jdbc:postgresql://$POSTGRES_HOST/$POSTGRES_DB?currentSchema=$SCHEMA" bash
+echo "constructed spring.datasource.url = $(echo '$spring.datasource.url' | awk  -f envSubstitution.awk)"
 VERSION=$(cat ./pom.xml | grep -m 1 '<version>' | awk -F"[><]" '{print $3}')
 export VERSION
 echo "Version: '$VERSION'"

6ff43214716bc9635f01d77e9b81f9d2c72dcfe4
xuemeigan <xuemei.gan@canada.ca>
2020-11-25 09:40:53 -0500

    Support 19944 Integrate seqdb-api with dina-base-api auditing. - Remove the AuditListener.SnapshotLoader -Use dina base audit serive for PcrPrimer and Region Dtos

--- src/main/java/ca/gc/aafc/seqdb/api/repository/PcrPrimerRepository.java
+++ src/main/java/ca/gc/aafc/seqdb/api/repository/PcrPrimerRepository.java
@@ -2,11 +2,14 @@ package ca.gc.aafc.seqdb.api.repository;
 
 import java.util.Optional;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Repository;
 
 import ca.gc.aafc.dina.filter.DinaFilterResolver;
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.repository.DinaRepository;
+import ca.gc.aafc.dina.service.AuditService;
 import ca.gc.aafc.dina.service.DinaAuthorizationService;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.seqdb.api.dto.PcrPrimerDto;
@@ -19,12 +22,13 @@ public class PcrPrimerRepository extends DinaRepository<PcrPrimerDto, PcrPrimer>
   public PcrPrimerRepository(
     @NonNull DinaService<PcrPrimer> dinaService,
     @NonNull DinaFilterResolver filterResolver,
-    Optional<DinaAuthorizationService> authService
+    Optional<DinaAuthorizationService> authService,
+    Optional<AuditService> auditService
   ) {
     super(
       dinaService,
       authService,
-      Optional.empty(),
+      auditService,
       new DinaMapper<>(PcrPrimerDto.class),
       PcrPrimerDto.class,
       PcrPrimer.class,

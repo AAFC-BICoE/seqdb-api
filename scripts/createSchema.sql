cc907bd1b122aa4318c0cb39da4e79b928b4382f
tom <tom.chalker@canada.ca>
2020-11-25 10:38:35 -0500

    Moved pgcrypto extension to 'setSearchPath.sql'

--- scripts/createSchema.sql
+++ scripts/createSchema.sql
@@ -7,5 +7,3 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA seqdb TO $spring_datasource_user
 
 alter default privileges in schema seqdb grant SELECT, INSERT, UPDATE, DELETE, REFERENCES on tables to $spring_datasource_username;
 alter default privileges in schema seqdb grant SELECT, USAGE ON sequences to $spring_datasource_username;
-
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA seqdb;
--- scripts/setSearchPath.sql
+++ scripts/setSearchPath.sql
@@ -1,2 +1,2 @@
 ALTER ROLE $spring_datasource_username SET search_path TO seqdb,public;
-
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA seqdb;

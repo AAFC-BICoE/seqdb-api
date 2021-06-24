699d01359b5292994686438c36447ec20ab29ab3
luusteve <luu.stevenluu@gmail.com>
2021-06-24 15:40:16 -0400

    rename PcrProfile to ThermocyclerProfile

--- src/main/java/ca/gc/aafc/seqdb/api/dto/ThermocyclerProfileDto.java
+++ src/main/java/ca/gc/aafc/seqdb/api/dto/ThermocyclerProfileDto.java
@@ -5,7 +5,7 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import ca.gc.aafc.dina.dto.RelatedEntity;
-import ca.gc.aafc.seqdb.api.entities.PcrProfile;
+import ca.gc.aafc.seqdb.api.entities.ThermocycleProfile;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiRelation;
@@ -15,7 +15,7 @@ import lombok.Data;
 @Data
 @JsonApiResource(type = "thermocyclerprofile")
 @SuppressFBWarnings(value = "EI_EXPOSE_REP")
-@RelatedEntity(PcrProfile.class)
+@RelatedEntity(ThermocycleProfile.class)
 public class ThermocyclerProfileDto {
 
   @JsonApiId
--- src/test/java/ca/gc/aafc/seqdb/api/testsupport/factories/ThermocyclerProfileFactory.java
+++ src/test/java/ca/gc/aafc/seqdb/api/testsupport/factories/ThermocyclerProfileFactory.java
@@ -3,25 +3,25 @@ package ca.gc.aafc.seqdb.api.testsupport.factories;
 import java.util.List;
 import java.util.function.BiFunction;
 
-import ca.gc.aafc.seqdb.api.entities.PcrProfile;
-import ca.gc.aafc.seqdb.api.entities.PcrProfile.PcrProfileBuilder;
+import ca.gc.aafc.seqdb.api.entities.ThermocycleProfile;
+import ca.gc.aafc.seqdb.api.entities.ThermocycleProfile.ThermocycleProfileBuilder;
 
-public class PcrProfileFactory implements TestableEntityFactory<PcrProfile> {
+public class ThermocyclerProfileFactory implements TestableEntityFactory<ThermocycleProfile> {
 
   @Override
-  public PcrProfile getEntityInstance() {
-    return newPcrProfile().build();
+  public ThermocycleProfile getEntityInstance() {
+    return newThermocycleProfile().build();
   }
   
   /**
    * Static method that can be called to return a configured builder that can be further customized
    * to return the actual entity object, call the .build() method on a builder.
-   * @param group Group to be set on the {@link PcrProfile}
+   * @param group Group to be set on the {@link ThermocycleProfile}
    * @return Pre-configured builder with all mandatory fields set
    */
-  public static PcrProfile.PcrProfileBuilder newPcrProfile() {
+  public static ThermocycleProfile.ThermocycleProfileBuilder newThermocycleProfile() {
     
-    return PcrProfile.builder()
+    return ThermocycleProfile.builder()
         .name(TestableEntityFactory.generateRandomName(10));
     
   }  
@@ -32,7 +32,7 @@ public class PcrProfileFactory implements TestableEntityFactory<PcrProfile> {
    * @param qty The number of PcrProfile populated in the list
    * @return List of PcrProfile
    */
-  public static List<PcrProfile> newListOf(int qty) {
+  public static List<ThermocycleProfile> newListOf(int qty) {
         
     return newListOf(qty, null);
   }
@@ -47,11 +47,11 @@ public class PcrProfileFactory implements TestableEntityFactory<PcrProfile> {
    *                      the list.
    * @return List of PcrProfiles
    */
-  public static List<PcrProfile> newListOf(int qty,
-      BiFunction<PcrProfile.PcrProfileBuilder, Integer, PcrProfile.PcrProfileBuilder> configuration) {
+  public static List<ThermocycleProfile> newListOf(int qty,
+      BiFunction<ThermocycleProfile.ThermocycleProfileBuilder, Integer, ThermocycleProfile.ThermocycleProfileBuilder> configuration) {
 
-    return TestableEntityFactory.newEntity(qty, PcrProfileFactory::newPcrProfile, configuration,
-        PcrProfileBuilder::build);
+    return TestableEntityFactory.newEntity(qty, ThermocyclerProfileFactory::newThermocycleProfile, configuration,
+      ThermocycleProfileBuilder::build);
 
   }
 

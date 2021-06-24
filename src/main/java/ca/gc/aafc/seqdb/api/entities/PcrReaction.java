981dd3aedc2ec1da7e65baef0e002ee31ea4cd6f
luusteve <luu.stevenluu@gmail.com>
2021-06-24 16:30:29 -0400

    create PcrReaction

---
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/PcrReaction.java
@@ -0,0 +1,72 @@
+package ca.gc.aafc.seqdb.api.entities;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+import org.hibernate.annotations.Type;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+@SuppressFBWarnings(justification = "ok for Hibernate Entity", value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@NaturalIdCache
+public class PcrReaction implements DinaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  @NotBlank
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Column(name = "_group")
+  private String group;
+
+  @Type(type = "list-array")
+  @Column(name = "attachment", columnDefinition = "uuid[]")
+  private List<UUID> attachment = new ArrayList<>();
+
+  @ManyToOne(cascade = {}, fetch = FetchType.LAZY)
+  @JoinColumn(name = "sample_id")
+  private MolecularSample sample;
+  
+}

d7b5a7ef313cac7873bd080a078c12b9f847e392
luusteve <luu.stevenluu@gmail.com>
2021-06-25 09:51:21 -0400

    checkstyle fix

--- src/main/java/ca/gc/aafc/seqdb/api/entities/PcrReaction.java
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/PcrReaction.java
@@ -1,7 +1,6 @@
 package ca.gc.aafc.seqdb.api.entities;
 
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;

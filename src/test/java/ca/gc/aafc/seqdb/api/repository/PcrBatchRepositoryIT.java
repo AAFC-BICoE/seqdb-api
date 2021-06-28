5822c6bfb711d28c70e0c726848631bbf5dbbb21
luusteve <luu.stevenluu@gmail.com>
2021-06-28 10:51:40 -0400

    migration file fix and IT modify

--- src/test/java/ca/gc/aafc/seqdb/api/repository/PcrBatchRepositoryIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/PcrBatchRepositoryIT.java
@@ -6,17 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.testsupport.security.WithMockKeycloakUser;
 import ca.gc.aafc.seqdb.api.dto.PcrBatchDto;
+import ca.gc.aafc.seqdb.api.dto.PcrPrimerDto;
+import ca.gc.aafc.seqdb.api.dto.RegionDto;
 import ca.gc.aafc.seqdb.api.entities.PcrBatch;
+import ca.gc.aafc.seqdb.api.entities.PcrPrimer.PrimerType;
 import ca.gc.aafc.seqdb.api.service.PcrBatchService;
 import ca.gc.aafc.seqdb.api.testsupport.factories.PcrBatchFactory;
 import ca.gc.aafc.seqdb.api.testsupport.fixtures.PcrBatchTestFixture;
@@ -31,10 +37,49 @@ public class PcrBatchRepositoryIT extends BaseRepositoryTest {
   @Inject
   protected PcrBatchRepository pcrBatchRepository;
 
+  @Inject
+  protected PcrPrimerRepository pcrPrimerRepository;
+
+  @Inject
+  protected RegionRepository regionRepository;
+
+  private PcrPrimerDto primerForwardTest;
+  private PcrPrimerDto primerReverseTest;
+  private RegionDto regionTest;
+
+  @BeforeEach
+  public void setup() {
+    PcrPrimerDto primerForward = new PcrPrimerDto();
+    primerForward.setType(PrimerType.PRIMER);
+    primerForward.setName("forward");
+    primerForward.setSeq("CTTGGTCATTTAGAGGAAGTAA");
+    primerForward.setDirection("F");
+    primerForward.setLotNumber(1);
+
+    PcrPrimerDto primerReverse = new PcrPrimerDto();
+    primerReverse.setType(PrimerType.PRIMER);
+    primerReverse.setName("reverse");
+    primerReverse.setSeq("CTTGGTCATTTAGAGGAAGTAA");
+    primerReverse.setDirection("R");
+    primerReverse.setLotNumber(1);
+
+    primerForwardTest = pcrPrimerRepository.create(primerForward);
+    primerReverseTest = pcrPrimerRepository.create(primerReverse);
+
+    RegionDto region = new RegionDto();
+    region.setSymbol("test");
+    region.setName("region");
+
+    regionTest = regionRepository.create(region);
+  }
+
   @Test
   public void findPcrBatch_whenPcrBatchExists_PcrBatchReturned() {
 
     PcrBatchDto newDto = PcrBatchTestFixture.newPcrBatch();
+    newDto.setPrimerForward(primerForwardTest);
+    newDto.setPrimerReverse(primerReverseTest);
+    newDto.setRegion(regionTest);
     
     PcrBatchDto created = pcrBatchRepository.create(newDto);
 
@@ -45,14 +90,24 @@ public class PcrBatchRepositoryIT extends BaseRepositoryTest {
     
     assertNotNull(found);
     assertEquals(created.getGroup(), found.getGroup());
+    assertEquals(primerForwardTest.getUuid(), found.getPrimerForward().getUuid());
+    assertEquals(primerReverseTest.getUuid(), found.getPrimerReverse().getUuid());
+    assertEquals(regionTest.getUuid(), found.getRegion().getUuid());
+
   }
   
   @Test
   public void createPcrBatch_onSuccess_PcrBatchCreated() {
     PcrBatchDto newDto = PcrBatchTestFixture.newPcrBatch();
+    newDto.setPrimerForward(primerForwardTest);
+    newDto.setPrimerReverse(primerReverseTest);
+    newDto.setRegion(regionTest);
     
     PcrBatchDto created = pcrBatchRepository.create(newDto);
     assertNotNull(created.getUuid());
+    assertEquals(primerForwardTest.getUuid(), created.getPrimerForward().getUuid());
+    assertEquals(primerReverseTest.getUuid(), created.getPrimerReverse().getUuid());
+    assertEquals(regionTest.getUuid(), created.getRegion().getUuid());
   }
   
   @Test
@@ -69,7 +124,9 @@ public class PcrBatchRepositoryIT extends BaseRepositoryTest {
 
     UUID uuid = UUID.randomUUID();
 
-    List<UUID> experimenters = List.of(uuid);
+    List<UUID> experimenters = new ArrayList<>();
+
+    experimenters.add(uuid);
     
     found.setExperimenters(experimenters);;
     PcrBatchDto updated = pcrBatchRepository.save(found);

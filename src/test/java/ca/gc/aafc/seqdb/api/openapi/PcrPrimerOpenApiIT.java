1dd03f6a2c1dcc2e78178855ffa5d6f746eeceb8
luusteve <luu.stevenluu@gmail.com>
2021-07-05 11:24:24 -0400

    update spec_path

--- src/test/java/ca/gc/aafc/seqdb/api/openapi/PcrPrimerOpenApiIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/openapi/PcrPrimerOpenApiIT.java
@@ -34,7 +34,7 @@ import lombok.SneakyThrows;
 public class PcrPrimerOpenApiIT extends BaseRestAssuredTest {
 
   private static final String SPEC_HOST = "raw.githubusercontent.com";
-  private static final String SPEC_PATH = "DINA-Web/sequence-specs/support_19623_Create_OpenAPI3_file_for_PcrPrimer_and_Region/schema/sequence.yml";
+  private static final String SPEC_PATH = "luusteve/sequence-specs/support_19623_Create_OpenAPI3_file_for_PcrPrimer_and_Region/schema/sequence.yml";
   private static final URIBuilder URI_BUILDER = new URIBuilder();
 
   public static final String TYPE_NAME = "pcr-primer";

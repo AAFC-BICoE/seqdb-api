95ccc7db3ac55f9f331e50a046fa2a3bbfcdc43c
luusteve <45112541+luusteve@users.noreply.github.com>
2021-07-05 12:54:01 -0400

    19623 create open api3 file for pcr primer and region (#149)
    * create openapiIT
    * update spec_path

--- src/test/java/ca/gc/aafc/seqdb/api/testsupport/fixtures/PcrBatchTestFixture.java
+++ src/test/java/ca/gc/aafc/seqdb/api/testsupport/fixtures/PcrBatchTestFixture.java
@@ -6,11 +6,12 @@ import ca.gc.aafc.seqdb.api.testsupport.factories.TestableEntityFactory;
 public class PcrBatchTestFixture {
 
   private static final String GROUP = "aafc";
+  private static final String CREATED_BY = "createdBy";
 
   public static PcrBatchDto newPcrBatch() {
     PcrBatchDto pcrBatchDto = new PcrBatchDto();
     pcrBatchDto.setGroup(GROUP);
-    pcrBatchDto.setCreatedBy("createdBy");
+    pcrBatchDto.setCreatedBy(CREATED_BY);
     pcrBatchDto.setName(TestableEntityFactory.generateRandomName(10));
     return pcrBatchDto;
   }

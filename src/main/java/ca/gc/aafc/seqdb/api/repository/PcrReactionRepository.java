015e3d6a85dfbb62b669b9eea7d94f4fd715581b
luusteve <45112541+luusteve@users.noreply.github.com>
2021-07-06 12:52:49 -0400

    update to dina-base 0.60 (#153)

--- src/main/java/ca/gc/aafc/seqdb/api/repository/PcrReactionRepository.java
+++ src/main/java/ca/gc/aafc/seqdb/api/repository/PcrReactionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.repository.DinaRepository;
 import ca.gc.aafc.dina.repository.external.ExternalResourceProvider;
-import ca.gc.aafc.dina.service.DinaAuthorizationService;
+import ca.gc.aafc.dina.security.DinaAuthorizationService;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.seqdb.api.dto.PcrReactionDto;
 import ca.gc.aafc.seqdb.api.entities.PcrReaction;

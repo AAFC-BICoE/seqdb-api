0b6838e4594c8971f6a073c8af344b9e555dd676
luusteve <luu.stevenluu@gmail.com>
2021-07-13 13:27:25 -0400

    checkstyle fix

--- src/main/java/ca/gc/aafc/seqdb/api/entities/PcrReaction.java
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/PcrReaction.java
@@ -12,7 +12,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;

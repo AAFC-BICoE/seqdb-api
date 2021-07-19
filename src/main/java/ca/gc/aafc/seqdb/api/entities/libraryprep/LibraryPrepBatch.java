ad3fa7627c9ca1486235152ab76b23cbb2e60778
luusteve <luu.stevenluu@gmail.com>
2021-07-19 11:38:02 -0400

    checkstyle fix

--- src/main/java/ca/gc/aafc/seqdb/api/entities/libraryprep/LibraryPrepBatch.java
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/libraryprep/LibraryPrepBatch.java
@@ -3,7 +3,6 @@ package ca.gc.aafc.seqdb.api.entities.libraryprep;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -17,7 +16,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
 
@@ -28,7 +26,6 @@ import ca.gc.aafc.seqdb.api.entities.ContainerType;
 import ca.gc.aafc.seqdb.api.entities.ThermocyclerProfile;
 import ca.gc.aafc.seqdb.api.entities.Product;
 import ca.gc.aafc.seqdb.api.entities.Protocol;
-import ca.gc.aafc.seqdb.api.entities.workflow.Chain;
 import ca.gc.aafc.seqdb.api.entities.workflow.StepResource;
 import lombok.AllArgsConstructor;
 import lombok.Builder;

6a0bc9556b960f89dc1cf80684bf19f9a34722e4
luusteve <luu.stevenluu@gmail.com>
2021-08-10 15:54:39 -0400

    add PcrBatchItem

---
+++ src/test/java/ca/gc/aafc/seqdb/api/testsupport/factories/PcrBatchItemFactory.java
@@ -0,0 +1,28 @@
+package ca.gc.aafc.seqdb.api.testsupport.factories;
+
+import java.util.UUID;
+
+import ca.gc.aafc.seqdb.api.entities.PcrBatchItem;
+
+public class PcrBatchItemFactory implements TestableEntityFactory<PcrBatchItem> {
+
+  @Override
+  public PcrBatchItem getEntityInstance() {
+    return newPcrBatchItem().build();
+  }
+    
+  /**
+   * Static method that can be called to return a configured builder that can be further customized
+   * to return the actual entity object, call the .build() method on a builder.
+   * @return Pre-configured builder with all mandatory fields set
+   */
+  public static PcrBatchItem.PcrBatchItemBuilder newPcrBatchItem() {
+    
+    return PcrBatchItem.builder()
+      .uuid(UUID.randomUUID())
+      .createdBy("test user")
+      .group("dina");
+    
+  }  
+  
+}

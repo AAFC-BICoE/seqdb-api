2b68dda7530ad20e2164738c3bb8997e62fcbb10
Steven Luu <45112541+luusteve@users.noreply.github.com>
2022-06-14 11:18:14 -0400

    25764 create seq submission (#181)
    Create SeqSubmission
    
    Co-authored-by: @chessfooty7 
    Co-authored-by: @cgendreau

---
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/SeqSubmission.java
@@ -0,0 +1,71 @@
+package ca.gc.aafc.seqdb.api.entities;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@SuppressFBWarnings(justification = "ok for Hibernate Entity", value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@NaturalIdCache
+@Table(name = "seq_submission")
+public class SeqSubmission implements DinaEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  @NotBlank
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Size(max = 50)
+  @Column(name = "_group")
+  private String group;
+
+  @NotBlank
+  @Size(max = 50)
+  private String name;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "seq_batch_id")
+  private SeqBatch seqBatch;
+
+  @Column(name = "submitted_by")
+  private UUID submittedBy;
+}
--- src/test/java/ca/gc/aafc/seqdb/api/SequenceModuleBaseIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/SequenceModuleBaseIT.java
@@ -3,10 +3,14 @@ package ca.gc.aafc.seqdb.api;
 import javax.inject.Inject;
 
 import ca.gc.aafc.seqdb.api.service.PcrBatchItemService;
+import ca.gc.aafc.seqdb.api.service.SeqSubmissionService;
 
 public class SequenceModuleBaseIT extends BaseIntegrationTest {
 
   @Inject
   protected PcrBatchItemService pcrBatchItemService;
+
+  @Inject 
+  protected SeqSubmissionService seqSubmissionService;
   
 }

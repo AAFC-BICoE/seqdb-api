085b552c2384979cd4b53d803b1e1aa4964e1def
Christian Gendreau <christiangendreau@gmail.com>
2022-07-21 14:07:56 -0400

    28295 Update dina-base and SpringBoot Updated dina-base to 0.91, Spring Boot to 2.7.2. Also updated SpotBugs and Checkstyle

--- src/main/java/ca/gc/aafc/seqdb/api/entities/StorageRestriction.java
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/StorageRestriction.java
@@ -1,13 +1,12 @@
 package ca.gc.aafc.seqdb.api.entities;
 
+import ca.gc.aafc.dina.entity.StorageGridLayout;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -22,39 +21,6 @@ public class StorageRestriction {
 
   @Valid
   @NotNull
-  private Layout layout;
-
-  @Data
-  @Builder
-  public static class Layout {
-
-    @NotNull
-    @Min(1)
-    @Max(1000)
-    private Integer numberOfRows;
-
-    @NotNull
-    @Min(1)
-    @Max(1000)
-    private Integer numberOfColumns;
-
-    @NotNull
-    private FillDirection fillDirection;
-
-    public enum FillDirection {
-      BY_ROW("by row"), BY_COLUMN("by column");
-
-      private final String text;
-
-      FillDirection(String text) {
-        this.text = text;
-      }
-
-      public String getText() {
-        return this.text;
-      }
-    }
-  }
-
+  private StorageGridLayout layout;
 
 }
--- src/test/java/ca/gc/aafc/seqdb/api/testsupport/factories/StorageRestrictionFactory.java
+++ src/test/java/ca/gc/aafc/seqdb/api/testsupport/factories/StorageRestrictionFactory.java
@@ -1,13 +1,14 @@
 package ca.gc.aafc.seqdb.api.testsupport.factories;
 
+import ca.gc.aafc.dina.entity.StorageGridLayout;
 import ca.gc.aafc.seqdb.api.entities.StorageRestriction;
 
 public class StorageRestrictionFactory {
 
   public static StorageRestriction.StorageRestrictionBuilder newStorageRestriction() {
     return StorageRestriction.builder()
-            .layout(StorageRestriction.Layout.builder()
-                    .fillDirection(StorageRestriction.Layout.FillDirection.BY_ROW)
+            .layout(StorageGridLayout.builder()
+                    .fillDirection(StorageGridLayout.FillDirection.BY_ROW)
                     .numberOfRows(8)
                     .numberOfColumns(12)
                     .build());

09599abd16215d1bd969d8ca002fff79503982d9
Christian Gendreau <christiangendreau@gmail.com>
2022-07-29 11:41:52 -0400

    28247 Replace ThermocyclerProfile steps by an array Removed the 15 step fields and added an array-based one

--- src/test/java/ca/gc/aafc/seqdb/api/repository/ThermocyclerResourceRepositoryIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/ThermocyclerResourceRepositoryIT.java
@@ -1,15 +1,17 @@
 package ca.gc.aafc.seqdb.api.repository;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ca.gc.aafc.dina.jpa.BaseDAO;
@@ -21,9 +23,7 @@ import io.crnk.core.queryspec.QuerySpec;
 
 public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
   
-  
   private static final String TEST_PROFILE_NAME = "test name";
-  
   private static final String TEST_PROFILE_CYCLE = "Cycle";
 
   @Inject
@@ -31,92 +31,37 @@ public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
 
   @Inject
   private ThermocyclerProfileRepository thermocyclerProfileRepository;
-  
-  private ThermocyclerProfile testThermocyclerProfile;
-  
+
   private ThermocyclerProfile createTestProfile() {
-    testThermocyclerProfile = ThermocyclerProfileFactory.newThermocyclerProfile()
+    ThermocyclerProfile testThermocyclerProfile = ThermocyclerProfileFactory.newThermocyclerProfile()
         .name(TEST_PROFILE_NAME)
         .cycles(TEST_PROFILE_CYCLE)
-        .step1("1")
-        .step2("2")
-        .step3("3")
-        .step4("4")
-        .step5("5")
-        .step6("6")
-        .step7("7")
-        .step8("8")
-        .step9("9")
-        .step10("10")
-        .step11("11")
-        .step12("12")
-        .step13("13")
-        .step14("14")
-        .step15("15")
+        .steps(List.of("step1", "step2"))
         .build();
-  persist(testThermocyclerProfile);
-    
-  return testThermocyclerProfile;
+    persist(testThermocyclerProfile);
+    return testThermocyclerProfile;
   }
-  
-  private void verifyStepsAreEqual(ThermocyclerProfile entity, ThermocyclerProfileDto dto) {
-    
-    assertEquals(dto.getStep1(), entity.getStep1());
-    assertEquals(dto.getStep2(), entity.getStep2());
-    assertEquals(dto.getStep3(), entity.getStep3());
-    assertEquals(dto.getStep4(), entity.getStep4());
-    assertEquals(dto.getStep5(), entity.getStep5());
-    assertEquals(dto.getStep6(), entity.getStep6());
-    assertEquals(dto.getStep7(), entity.getStep7());
-    assertEquals(dto.getStep8(), entity.getStep8());
-    assertEquals(dto.getStep9(), entity.getStep9());
-    assertEquals(dto.getStep10(), entity.getStep10());
-    assertEquals(dto.getStep11(), entity.getStep11());
-    assertEquals(dto.getStep12(), entity.getStep12());
-    assertEquals(dto.getStep13(), entity.getStep13());
-    assertEquals(dto.getStep14(), entity.getStep14());
-    assertEquals(dto.getStep15(), entity.getStep15());
 
-  }
-  
-  private void verifyStepsAreEqual(ThermocyclerProfileDto baseDto, ThermocyclerProfileDto newDto) {
-    
-    assertEquals(newDto.getStep1(), baseDto.getStep1());
-    assertEquals(newDto.getStep2(), baseDto.getStep2());
-    assertEquals(newDto.getStep3(), baseDto.getStep3());
-    assertEquals(newDto.getStep4(), baseDto.getStep4());
-    assertEquals(newDto.getStep5(), baseDto.getStep5());
-    assertEquals(newDto.getStep6(), baseDto.getStep6());
-    assertEquals(newDto.getStep7(), baseDto.getStep7());
-    assertEquals(newDto.getStep8(), baseDto.getStep8());
-    assertEquals(newDto.getStep9(), baseDto.getStep9());
-    assertEquals(newDto.getStep10(), baseDto.getStep10());
-    assertEquals(newDto.getStep11(), baseDto.getStep11());
-    assertEquals(newDto.getStep12(), baseDto.getStep12());
-    assertEquals(newDto.getStep13(), baseDto.getStep13());
-    assertEquals(newDto.getStep14(), baseDto.getStep14());
-    assertEquals(newDto.getStep15(), baseDto.getStep15());
 
-  }
-  
-  @BeforeEach
-  public void setup() {
-    createTestProfile();
-  }
-  
   @Test
   public void findThermocyclerProfile_whenNoFieldsAreSelected_productReturnedWithAllFields() {
+
+    ThermocyclerProfile testThermocyclerProfile = createTestProfile();
+
     ThermocyclerProfileDto thermoDto = thermocyclerProfileRepository.findOne(testThermocyclerProfile.getUuid(), new QuerySpec(ThermocyclerProfileDto.class));
     assertNotNull(thermoDto);
     assertEquals(testThermocyclerProfile.getUuid(), thermoDto.getUuid());
     assertEquals(TEST_PROFILE_NAME, thermoDto.getName());
     assertEquals(TEST_PROFILE_CYCLE, thermoDto.getCycles());
-    verifyStepsAreEqual(testThermocyclerProfile, thermoDto);
 
+    System.out.println(testThermocyclerProfile.getSteps());
+    System.out.println(thermoDto.getSteps());
+
+    assertThat(testThermocyclerProfile.getSteps(), contains(thermoDto.getSteps().toArray()));
   }
   
   @Test
-  public void createAndPersistThermocyclerProfile_onSucess_allFieldsHavePersistedValues() {
+  public void createAndPersistThermocyclerProfile_onSuccess_allFieldsHavePersistedValues() {
     String newThermocyclerProfileName = "new thermocycler profile";
 
     //set a base DTO
@@ -131,21 +76,19 @@ public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
     assertNotNull(createdDto.getUuid());
     assertEquals(newThermocyclerProfileName, createdDto.getName());
     assertEquals(TEST_PROFILE_CYCLE, createdDto.getCycles());
-    verifyStepsAreEqual(baseDto, createdDto);
+    assertNull(createdDto.getSteps());
     
     //Assert Entity has the set values
     ThermocyclerProfile profileEntity = baseDao.findOneByNaturalId(createdDto.getUuid(), ThermocyclerProfile.class);
     assertNotNull(profileEntity.getId());
     assertEquals(newThermocyclerProfileName, profileEntity.getName());
     assertEquals(TEST_PROFILE_CYCLE, profileEntity.getCycles());
-    verifyStepsAreEqual(profileEntity, baseDto);
-      
-    
+    assertNull(profileEntity.getSteps());
   }
   
   @Test
   public void updateThermocyclerProfile_dtoWithOnlyUpdatedFields_entityReturnedWithUpdatedFields() {
-    
+    ThermocyclerProfile testThermocyclerProfile = createTestProfile();
     QuerySpec querySpec = new QuerySpec(ThermocyclerProfileDto.class);
     
     ThermocyclerProfileDto thermoDto = thermocyclerProfileRepository.findOne(testThermocyclerProfile.getUuid(), querySpec);
@@ -160,9 +103,9 @@ public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
 
   @Test
   public void deleteThermocyclerProfile_callRepositoryDeleteOnID_profileNotFound() {
+    ThermocyclerProfile testThermocyclerProfile = createTestProfile();
     thermocyclerProfileRepository.delete(testThermocyclerProfile.getUuid());
     assertNull(entityManager.find(ThermocyclerProfile.class, testThermocyclerProfile.getId()));
-    
   }
 
   @Test

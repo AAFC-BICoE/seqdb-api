62a4364ad9425ca08623c9c4ffe21bf3010aa36d
Christian Gendreau <christiangendreau@gmail.com>
2022-07-29 15:30:31 -0400

    Improved ThermocyclerProfile tests

--- src/test/java/ca/gc/aafc/seqdb/api/repository/ThermocyclerResourceRepositoryIT.java
+++ src/test/java/ca/gc/aafc/seqdb/api/repository/ThermocyclerResourceRepositoryIT.java
@@ -11,37 +11,50 @@ import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
 
+import ca.gc.aafc.seqdb.api.service.ThermocyclerProfileService;
+import ca.gc.aafc.seqdb.api.testsupport.fixtures.ThermocyclerProfileTestFixture;
 import org.junit.jupiter.api.Test;
 
-import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.seqdb.api.dto.ThermocyclerProfileDto;
 import ca.gc.aafc.seqdb.api.entities.ThermocyclerProfile;
 import ca.gc.aafc.seqdb.api.testsupport.factories.ThermocyclerProfileFactory;
 import io.crnk.core.exception.ResourceNotFoundException;
 import io.crnk.core.queryspec.QuerySpec;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
 
 public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
   
   private static final String TEST_PROFILE_NAME = "test name";
-  private static final String TEST_PROFILE_CYCLE = "Cycle";
+  private static final String TEST_PROFILE_CYCLE = ThermocyclerProfileTestFixture.TEST_PROFILE_CYCLE;
 
   @Inject
-  private BaseDAO baseDao;
+  private ThermocyclerProfileService thermocyclerProfileService;
 
   @Inject
   private ThermocyclerProfileRepository thermocyclerProfileRepository;
 
-  private ThermocyclerProfile createTestProfile() {
-    ThermocyclerProfile testThermocyclerProfile = ThermocyclerProfileFactory.newThermocyclerProfile()
-        .name(TEST_PROFILE_NAME)
-        .cycles(TEST_PROFILE_CYCLE)
-        .steps(List.of("step1", "step2"))
-        .build();
-    persist(testThermocyclerProfile);
-    return testThermocyclerProfile;
+
+  private static ThermocyclerProfile.ThermocyclerProfileBuilder initTestProfileBuilder() {
+    return ThermocyclerProfileFactory.newThermocyclerProfile()
+            .name(TEST_PROFILE_NAME)
+            .cycles(TEST_PROFILE_CYCLE)
+            .steps(List.of("step1", "step2"));
   }
 
+  private ThermocyclerProfile createTestProfile() {
+    ThermocyclerProfile testThermocyclerProfile = initTestProfileBuilder()
+            .build();
+    return thermocyclerProfileService.create(testThermocyclerProfile);
+  }
+
+  @Test
+  public void createThermocyclerProfile_whenStepsSizeExceeded_ExceptionThrown() {
+    ThermocyclerProfileDto tpDto = ThermocyclerProfileTestFixture.newThermocyclerProfile();
+    tpDto.setSteps(List.of(RandomStringUtils.randomAlphabetic(1000)));
+    assertThrows(ConstraintViolationException.class, () -> thermocyclerProfileRepository.create(tpDto));
+  }
 
   @Test
   public void findThermocyclerProfile_whenNoFieldsAreSelected_productReturnedWithAllFields() {
@@ -54,36 +67,30 @@ public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
     assertEquals(TEST_PROFILE_NAME, thermoDto.getName());
     assertEquals(TEST_PROFILE_CYCLE, thermoDto.getCycles());
 
-    System.out.println(testThermocyclerProfile.getSteps());
-    System.out.println(thermoDto.getSteps());
-
     assertThat(testThermocyclerProfile.getSteps(), contains(thermoDto.getSteps().toArray()));
   }
   
   @Test
   public void createAndPersistThermocyclerProfile_onSuccess_allFieldsHavePersistedValues() {
-    String newThermocyclerProfileName = "new thermocycler profile";
 
     //set a base DTO
-    ThermocyclerProfileDto baseDto = new ThermocyclerProfileDto();
-    baseDto.setName(newThermocyclerProfileName);
-    baseDto.setCycles(TEST_PROFILE_CYCLE);
+    ThermocyclerProfileDto baseDto = ThermocyclerProfileTestFixture.newThermocyclerProfile();
     
     //create the DTO in the repository
     ThermocyclerProfileDto createdDto = thermocyclerProfileRepository.create(baseDto);
     
     //Assert DTO has the set values
     assertNotNull(createdDto.getUuid());
-    assertEquals(newThermocyclerProfileName, createdDto.getName());
+    assertEquals(baseDto.getName(), createdDto.getName());
     assertEquals(TEST_PROFILE_CYCLE, createdDto.getCycles());
-    assertNull(createdDto.getSteps());
+    assertThat(createdDto.getSteps(), contains(ThermocyclerProfileTestFixture.TEST_STEPS.toArray()));
     
     //Assert Entity has the set values
-    ThermocyclerProfile profileEntity = baseDao.findOneByNaturalId(createdDto.getUuid(), ThermocyclerProfile.class);
+    ThermocyclerProfile profileEntity = thermocyclerProfileService.findOne(createdDto.getUuid(), ThermocyclerProfile.class);
     assertNotNull(profileEntity.getId());
-    assertEquals(newThermocyclerProfileName, profileEntity.getName());
+    assertEquals(baseDto.getName(), profileEntity.getName());
     assertEquals(TEST_PROFILE_CYCLE, profileEntity.getCycles());
-    assertNull(profileEntity.getSteps());
+    assertThat(profileEntity.getSteps(), contains(ThermocyclerProfileTestFixture.TEST_STEPS.toArray()));
   }
   
   @Test
@@ -98,7 +105,6 @@ public class ThermocyclerResourceRepositoryIT extends BaseRepositoryTest {
     thermocyclerProfileRepository.save(thermoDto);
     
     assertEquals("new cycles", testThermocyclerProfile.getCycles());
-    
   }
 
   @Test

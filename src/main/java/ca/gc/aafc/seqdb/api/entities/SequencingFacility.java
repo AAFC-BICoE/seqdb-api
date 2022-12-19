f80299a7488f53ccec0735dc78814cd0edde2f9f
Christian Gendreau <christiangendreau@gmail.com>
2022-12-19 13:53:52 -0500

    29527 Add Sequencing Facility Added db migration + entity + crud test

---
+++ src/main/java/ca/gc/aafc/seqdb/api/entities/SequencingFacility.java
@@ -0,0 +1,56 @@
+package ca.gc.aafc.seqdb.api.entities;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@NaturalIdCache
+@Table(name = "sequencing_facility")
+public class SequencingFacility implements DinaEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NaturalId
+  @NotNull
+  @Column(unique = true)
+  private UUID uuid;
+
+  @NotBlank
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @Column(name = "created_on", insertable = false, updatable = false)
+  @Generated(value = GenerationTime.INSERT)
+  private OffsetDateTime createdOn;
+
+  @NotBlank
+  @Size(max = 50)
+  @Column(name = "_group")
+  private String group;
+
+  @NotBlank
+  @Size(max = 50)
+  private String name;
+}

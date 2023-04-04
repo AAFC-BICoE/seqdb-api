eb17164797fe4ca501ccf518f2809a04b8562f2e
Christian Gendreau <cgendreau@users.noreply.github.com>
2023-04-04 11:47:29 -0400

    30269 add seqsubmission dto and resource (#212)
    * 30269 Add seqsubmission dto and resource
    Added SeqSubmissionDto
    
    * 30269 Add seqsubmission dto and resource
    Added repository and testing

---
+++ src/main/java/ca/gc/aafc/seqdb/api/dto/SeqSubmissionDto.java
@@ -0,0 +1,42 @@
+package ca.gc.aafc.seqdb.api.dto;
+
+import org.javers.core.metamodel.annotation.ShallowReference;
+import org.javers.core.metamodel.annotation.TypeName;
+
+import ca.gc.aafc.dina.dto.ExternalRelationDto;
+import ca.gc.aafc.dina.dto.RelatedEntity;
+import ca.gc.aafc.dina.repository.meta.JsonApiExternalRelation;
+import ca.gc.aafc.seqdb.api.entities.SeqSubmission;
+
+import io.crnk.core.resource.annotations.JsonApiId;
+import io.crnk.core.resource.annotations.JsonApiRelation;
+import io.crnk.core.resource.annotations.JsonApiResource;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.Data;
+
+@Data
+@JsonApiResource(type = SeqSubmissionDto.TYPENAME)
+@RelatedEntity(SeqSubmission.class)
+@TypeName(SeqSubmissionDto.TYPENAME)
+public class SeqSubmissionDto {
+
+  public static final String TYPENAME = "seq-submission";
+
+  @JsonApiId
+  private UUID uuid;
+
+  private String group;
+  private String name;
+  private String createdBy;
+  private OffsetDateTime createdOn;
+
+  @ShallowReference
+  @JsonApiRelation
+  private SeqBatchDto seqBatch;
+
+  @JsonApiExternalRelation(type = "person")
+  @JsonApiRelation
+  private ExternalRelationDto submittedBy;
+
+}

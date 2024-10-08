8f663000efbf0b2b8e010cd31248509cefd0b253
Robert Gabriel Macalintal <169496244+rgmacalintal@users.noreply.github.com>
2024-10-08 16:01:29 -0400

    Support #35013 - Add OpenAPI specs to Integration Test for IndexSet (#240)
    * Added integrated test and corresponding fixture to appropriate directories
    * Rearranged fixture initializations to match dto declarations
    * Added TYPENAME

---
+++ src/test/java/ca/gc/aafc/seqdb/api/openapi/IndexSetOpenApiIT.java
@@ -0,0 +1,51 @@
+package ca.gc.aafc.seqdb.api.openapi;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import ca.gc.aafc.dina.testsupport.BaseRestAssuredTest;
+import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
+import ca.gc.aafc.dina.testsupport.jsonapi.JsonAPITestHelper;
+import ca.gc.aafc.dina.testsupport.specs.OpenAPI3Assertions;
+import ca.gc.aafc.dina.testsupport.specs.ValidationRestrictionOptions;
+import ca.gc.aafc.seqdb.api.SeqdbApiLauncher;
+import ca.gc.aafc.seqdb.api.dto.IndexSetDto;
+import ca.gc.aafc.seqdb.api.testsupport.fixtures.IndexSetTestFixture;
+import lombok.SneakyThrows;
+
+@SpringBootTest(
+  classes = SeqdbApiLauncher.class,
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+@ContextConfiguration(initializers = {PostgresTestContainerInitializer.class})
+public class IndexSetOpenApiIT extends BaseRestAssuredTest {
+
+  protected IndexSetOpenApiIT() {
+    super("/api");
+  }
+
+  @SneakyThrows
+  @Test
+  void indexSet_SpecValid() {
+    IndexSetDto indexSet = IndexSetTestFixture.newIndexSet();
+
+    OpenAPI3Assertions.assertRemoteSchema(
+      OpenAPIConstants.SEQDB_API_SPECS_URL,
+      "IndexSet",
+      sendPost(
+        IndexSetDto.TYPENAME, 
+        JsonAPITestHelper.toJsonAPIMap(
+          IndexSetDto.TYPENAME, 
+          JsonAPITestHelper.toAttributeMap(indexSet),
+          null,
+          null
+        )
+      ).extract().asString(),
+      ValidationRestrictionOptions.builder().allowableMissingFields(Set.of("ngsIndexes")).build()
+    );
+  }
+}

b37e88b71637fa8d9c4d105d2895cb1c26a21b12
Robert Gabriel Macalintal <robertgabriel.macalintal@agr.gc.ca>
2024-10-08 11:02:36 -0400

    Added integrated test and corresponding fixture to appropriate directories

---
+++ src/test/java/ca/gc/aafc/seqdb/api/testsupport/fixtures/IndexSetTestFixture.java
@@ -0,0 +1,26 @@
+package ca.gc.aafc.seqdb.api.testsupport.fixtures;
+
+import java.time.OffsetDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+
+import ca.gc.aafc.seqdb.api.dto.IndexSetDto;
+import ca.gc.aafc.seqdb.api.testsupport.factories.TestableEntityFactory;
+
+public class IndexSetTestFixture {
+    
+    public static IndexSetDto newIndexSet() {
+        IndexSetDto indexSetDto = new IndexSetDto();
+
+        indexSetDto.setName(TestableEntityFactory.generateRandomName(10));
+        indexSetDto.setGroup("aafc");
+        indexSetDto.setCreatedBy("createdBy");
+        indexSetDto.setCreatedOn(OffsetDateTime.of(LocalDate.now(), LocalTime.now(), ZoneOffset.MIN));
+        indexSetDto.setForwardAdapter("forwardAdapter");
+        indexSetDto.setReverseAdapter("reverseAdapter");
+        indexSetDto.setNgsIndexes(null);
+        
+        return indexSetDto;
+    }
+}

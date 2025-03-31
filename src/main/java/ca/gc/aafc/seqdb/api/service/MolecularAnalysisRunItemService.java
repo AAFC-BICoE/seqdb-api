75f5b9d271ee47e9470893cdca69b80c01ed4222
Christian Gendreau <christiangendreau@gmail.com>
2025-03-31 15:38:01 -0400

    35985 MolecularAnalysisRunItemService should emit messages Added messages support to MolecularAnalysisRunItemService

--- src/main/java/ca/gc/aafc/seqdb/api/service/MolecularAnalysisRunItemService.java
+++ src/main/java/ca/gc/aafc/seqdb/api/service/MolecularAnalysisRunItemService.java
@@ -7,16 +7,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.SmartValidator;
 
 import ca.gc.aafc.dina.jpa.BaseDAO;
-import ca.gc.aafc.dina.service.DefaultDinaService;
+import ca.gc.aafc.dina.messaging.DinaEventPublisher;
+import ca.gc.aafc.dina.messaging.EntityChanged;
+import ca.gc.aafc.dina.messaging.message.DocumentOperationType;
+import ca.gc.aafc.dina.service.MessageProducingService;
+import ca.gc.aafc.seqdb.api.dto.RunSummaryDto;
 import ca.gc.aafc.seqdb.api.entities.MolecularAnalysisRunItem;
 
 @Service
-public class MolecularAnalysisRunItemService extends DefaultDinaService<MolecularAnalysisRunItem> {
+public class MolecularAnalysisRunItemService extends MessageProducingService<MolecularAnalysisRunItem> {
+
+  // use the type summary since this is what is indexed
+  public static final String MESSAGE_TYPENAME = RunSummaryDto.TYPENAME;
 
   public MolecularAnalysisRunItemService(
     @NonNull BaseDAO baseDAO,
-    @NonNull SmartValidator sv) {
-    super(baseDAO, sv);
+    @NonNull SmartValidator sv,
+    DinaEventPublisher<EntityChanged> eventPublisher) {
+    super(baseDAO, sv, null, eventPublisher);
   }
 
   @Override
@@ -24,4 +32,16 @@ public class MolecularAnalysisRunItemService extends DefaultDinaService<Molecula
     entity.setUuid(UUID.randomUUID());
   }
 
+  @Override
+  protected void triggerEvent(MolecularAnalysisRunItem persisted, DocumentOperationType op) {
+    if (persisted.getRun() != null) {
+      EntityChanged event = EntityChanged.builder()
+        .op(DocumentOperationType.REFRESH)
+        .resourceType(MESSAGE_TYPENAME)
+        .uuid(persisted.getRun().getUuid())
+        .build();
+
+      publishEvent(event);
+    }
+  }
 }
